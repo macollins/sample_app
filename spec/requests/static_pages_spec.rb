db89740ce6175d41678ba467454f1993d3cace49
Michael Collins <macollins88@gmail.com>
2013-05-21 22:17:56 -0700

    DRY out static pages specs

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,65 +4,40 @@ describe "Static pages" do
 
   let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
-  describe "About page" do
-    it "should have the heading 'About Us'" do
-      visit about_path
-      page.should have_selector('h1',
-                                text: 'About Us')
-    end
+  subject { page }
 
-    it "should have the title 'About Us'" do
-      visit about_path
-      page.should have_selector('title',
-                                text: "#{base_title} | About Us")
-    end
+  describe "About page" do
+    before { visit about_path }
+
+    it { should have_selector('h1', text: 'About Us') }
+
+    it { should have_selector('title', text: "#{base_title} | About Us") }
   end
 
   describe "Contact page" do
-    it "should have the heading 'Contact'" do
-      visit contact_path
-      page.should have_selector('h1',
-                                text: 'Contact')
-    end
+    before { visit contact_path }
 
-    it "should have the title 'Contact'" do
-      visit contact_path
-      page.should have_selector('title',
-                                text: "#{base_title} | Contact")
-    end
+    it { should have_selector('h1', text: 'Contact') }
+
+    it { should have_selector('title', text: "#{base_title} | Contact") }
   end
 
   describe "Help page" do
-    it "should have the heading 'Help'" do
-      visit help_path
-      page.should have_selector('h1',
-                                text: 'Help')
-    end
+    before { visit help_path }
 
-    it "should have the title 'Help'" do
-      visit help_path
-      page.should have_selector('title',
-                                text: "#{base_title} | Help")
-    end
+    it { should have_selector('h1', text: 'Help') }
+
+    it { should have_selector('title', text: "#{base_title} | Help") }
   end
 
   describe "Home page" do
-    it "should have the heading 'Sample App'" do
-      visit root_path
-      page.should have_selector('h1', text: 'Sample App')
-    end
+    before { visit root_path }
 
-    it "should have the base title" do
-      visit root_path
-      page.should have_selector('title',
-                                text: "#{base_title}")
-    end
+    it { should have_selector('h1', text: 'Sample App') }
 
-    it "should not have a custom page title" do
-      visit root_path
-      page.should_not have_selector('title',
-                                    text: '| Home')
-    end
+    it { should have_selector('title', text: "#{base_title}") }
+
+    it { should_not have_selector('title', text: '| Home') }
   end
 
 end
