77b9921b8f5ff62eace213246b5303a7de51904e
Michael Collins <macollins88@gmail.com>
2013-05-31 23:02:03 -0700

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ SampleApp::Application.routes.draw do
   match '/help',    to: 'static_pages#help'
   match '/signup',  to: 'users#new'
 
+  resources :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
