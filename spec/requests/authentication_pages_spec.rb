4f094d49ed6756b05a1c3e6131b2157fc2c2f242
Michael Collins <macollins88@gmail.com>
2013-06-02 20:42:57 -0700

    Refactor specs

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -8,8 +8,8 @@ describe "Authentication" do
 
     before { visit signin_path }
 
-    it { should have_selector('h1',    text: 'Sign in') }
-    it { should have_selector('title', text: 'Sign in') }
+    it { should have_heading('Sign in') }
+    it { should have_title('Sign in') }
 
   end
 
@@ -20,12 +20,12 @@ describe "Authentication" do
     describe "with invalid credentials" do
       before { click_button "Sign in" }
 
-      it { should have_selector('title', text: 'Sign in') }
-      it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+      it { should have_title('Sign in') }
+      it { should have_error_message('Invalid') }
 
       describe "after visiting another page" do
         before { click_link "Home" }
-        it { should_not have_selector('div.alert.alert-error', text: 'Invalid') }
+        it { should_not have_error_message('Invalid') }
       end
     end
 
@@ -34,12 +34,10 @@ describe "Authentication" do
       let(:user) { FactoryGirl.create(:user) }
 
       before do
-        fill_in "Email", with: user.email.upcase
-        fill_in "Password", with: user.password
-        click_button "Sign in"
+        valid_signin(user)
       end
 
-      it { should have_selector('title', text: user.name) }
+      it { should have_title(user.name) }
       it { should have_link('Profile', href: user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
